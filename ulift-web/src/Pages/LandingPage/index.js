c3c0d8e56d3871fe0e44d04efaa6ececd182647b
Gabriel <gabrielvdrumondv@gmail.com>
2020-03-25 17:04:28 -0300

    finaliza primaira parte da landing page

--- ulift-web/src/Pages/LandingPage/index.js
+++ ulift-web/src/Pages/LandingPage/index.js
@@ -6,36 +6,39 @@ import Direction from "./../../assets/Direction.svg"
 function App() {
   return (
     <>
-      <div className="first__part">
-        <div className="container">
-          <div className="row">
-            <h1 className="margin__top text-center font-weight-bold">
-              uLift é uma aplicação perfeita para encontrar carona para chegar a tempo na universidade
-            </h1>
-          </div>
-
-          <div className="margin__top row d-flex justify-content-center">
-            <div className="col-12 col-lg-6 text-justify">
-              <h2>Procure facilmente pela carona mais próxima de acordo com a sua localização.</h2>
+      <div className="parts">
+        <div className="first__part padding-strict">
+          <div className="container">
+            <div className="row">
+              <h1 className="margin__top text-center font-weight-bold">
+                uLift é uma aplicação perfeita para encontrar carona para chegar a tempo na universidade
+              </h1>
             </div>
 
-            <div className="col-12 col-lg-4">
-              <img className="direction__img" src={Direction} alt="Caronas" />
+            <div className="margin__top row d-flex justify-content-center">
+              <div className="col-12 col-lg-5">
+                <h3>Procure facilmente pela carona mais próxima de acordo com a sua localização.</h3>
+              </div>
+
+              <div className="col-12 col-lg-3">
+                <img className="direction__img" src={Direction} alt="Caronas" />
+              </div>
             </div>
           </div>
-
         </div>
-      </div>
 
-      <div className="container-fluid second__part">
-        <div className="container">
-
+        <div className="second__part padding-strict">
+          <div className="container">
+            <div className="col-12 col-lg-4"></div>
+            <div className="col-12 col-lg-4"></div>
+            <div className="col-12 col-lg-4"></div>
+          </div>
         </div>
-      </div>
 
-      <div className="container-fluid third__part">
-        <div className="container">
+        <div className="third__part padding-strict">
+          <div className="container">
 
+          </div>
         </div>
       </div>
     </>
