c95740d10eadeb67f67616f59b5740ad073d7adc
Gabriel <gabrielvdrumondv@gmail.com>
2020-03-26 00:02:35 -0300

    adiciona segunda e terceira parte da landing page

--- ulift-web/src/Pages/LandingPage/index.js
+++ ulift-web/src/Pages/LandingPage/index.js
@@ -2,26 +2,30 @@ import React from "react";
 
 import "./styles.css"
 import Direction from "./../../assets/Direction.svg"
+import Pessoas from "./../../assets/Pessoas.svg"
+import Dinheiro from "./../../assets/Dinheiro.svg"
+import Tempo from "./../../assets/Tempo.svg"
+import Group from "./../../assets/Group.svg"
 
 function App() {
   return (
     <>
       <div className="parts">
-        <div className="first__part padding-strict">
-          <div className="container">
+        <div className="first__part padding-strict pt-4">
+          <div className="container margins">
             <div className="row">
-              <h1 className="margin__top text-center font-weight-bold">
-                uLift é uma aplicação perfeita para encontrar carona para chegar a tempo na universidade
+              <h1 className="text-center font-weight-bold">
+                uLift é a aplicação perfeita para encontrar carona e chegar a tempo na sua faculdade
               </h1>
             </div>
 
             <div className="margin__top row d-flex justify-content-center">
-              <div className="col-12 col-lg-5">
-                <h3>Procure facilmente pela carona mais próxima de acordo com a sua localização.</h3>
+              <div className="col-12 col-lg-6 text-justify">
+                <h3>Uma plataforma de fácil uso, para estudantes conseguirem uma carona de acordo com sua localização.</h3>
               </div>
 
-              <div className="col-12 col-lg-3">
-                <img className="direction__img" src={Direction} alt="Caronas" />
+              <div className="col-12 col-lg-4 text-center">
+                <img className="images image-direction" src={Direction} alt="Caronas" />
               </div>
             </div>
           </div>
@@ -29,15 +33,50 @@ function App() {
 
         <div className="second__part padding-strict">
           <div className="container">
-            <div className="col-12 col-lg-4"></div>
-            <div className="col-12 col-lg-4"></div>
-            <div className="col-12 col-lg-4"></div>
+            <div className="row margins justify-content-md-center text-center">
+              <div className="col-12 pb-5">
+                <h1>E quais as vantagens?</h1>
+              </div>
+
+              <div className="col-12 col-lg-4">
+                <img className="images" src={Tempo} alt="Tempo" />
+
+                <h4 className="pt-4 pb-0">Otimizar seu tempo</h4>
+              </div>
+
+              <div className="col-12 col-lg-4">
+                <img className="images" src={Pessoas} alt="Pessoas" />
+
+                <h4 className="pt-4 pb-0">Conhecer novas pessoas</h4>
+              </div>
+
+              <div className="col-12 col-lg-4">
+                <img className="images" src={Dinheiro} alt="Dinheiro" />
+
+                <h4 className="pt-4 pb-0">Economizar algum dinheiro</h4>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className="third__part padding-strict">
-          <div className="container">
 
+        <div className="third__part padding-strict">
+          <div className="container text-center">
+            <div className="row margins">
+              <div className="col-12">
+                <img className="images image__Group" src={Group} alt="Carro ao luar" />
+              </div>
+
+              <div className="col-12 text-white">
+                <h2>Você também pode oferecer caronas e economizar no seu combustível.</h2>
+
+                <button className="col-12 col-xl-6 btn btn-lg btn-light font-weight-bold primary__button">
+                  Começar a esperiencia
+                </button>
+
+                <h5 className="pt-3">Já possui conta? <a className="sign-link text-decoration-none" href="#">Entrar</a></h5>
+              </div>
+            </div>
           </div>
         </div>
       </div>
