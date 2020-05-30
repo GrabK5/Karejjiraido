3624d5333d007a8303814b10080458558f540356
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2020-05-30 17:37:38 -0300

    dashboard

---
+++ ulift-web/src/Pages/Dashboard/index.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import io from 'socket.io-client';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+import "./styles.css";
+import Header from "../../components/Header";
+import Haversine from '../../util/Haversine';
+
+const mapStyles = {
+  width: '100%',
+  height: '70%'
+};
+
+const Dashboard = props => {
+  const host = `ws://${window.location.hostname}:4000`;
+  const socket = useState(io(host, { transports: ['websocket'] }))[0];
+  const [users, setUsers] = useState([]);
+  const [position, setPosition] = useState(null);
+
+  const updateLocation = (pos) => {
+    const location = {
+      lat: pos.coords.latitude,
+      long: pos.coords.longitude,
+    }
+
+    setPosition(location);
+    const id = localStorage.getItem("@ulift");
+    const user = { id, location }
+    socket.emit("send_location", user);
+  }
+
+  const calculateDistance = user => {
+    const h = new Haversine(position, user.location);
+    return h.distance;
+  }
+
+  const errorHandler = (e) => {
+    if (e.code === 1) console.log("Acesso negado");
+    else if (e.code === 2) console.log("Posição inválida");
+  }
+
+  navigator.geolocation.watchPosition(updateLocation, errorHandler, { timeout: 3000 });
+
+  const displayUsersMarkers = () => {
+    if (position) {
+      return users.map((user) => {
+        return(<Marker key={user.id} position={{
+          lat: user.location.lat,
+          lng: user.location.long,
+        }} />);
+      });
+    }
+
+    return null;
+  };
+
+  const displayMyMarker = () => {
+    return(<Marker key={localStorage.getItem("@ulift")} position={{
+      lat: position.lat,
+      lng: position.long,
+    }} />)
+  }
+
+  socket.on("user_update", (data) => {
+    setUsers(data);
+  });
+
+  const renderMap = () => {
+    if (position !== null) {
+      return (<Map
+        google={props.google}
+        zoom={15}
+        style={mapStyles}
+        initialCenter={{
+          lat: position.lat,
+          lng: position.long,
+        }}
+      >
+        {displayMyMarker()}
+        {displayUsersMarkers()}
+      </Map>)
+    }
+    return null;
+  }
+
+  return (
+    <>
+      <Header />
+      <div>
+        <div id='lista'>
+          {users.map(user => (
+            <li key={user.id}>
+              {`${user.name} - distance: ${calculateDistance(user) / 1000} km`}
+            </li>
+          ))}
+        </div>
+        {renderMap()}
+      </div>
+    </>
+  );
+}
+
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_API_KEY,
+})(Dashboard);
